2042fef8f116a22f5ed7365f0366eb3c4c8e5ce9
pli01 <pli01@users.noreply.github.com>
2016-06-21 11:22:25 +0200

    fix url

--- base-image/build.sh
+++ base-image/build.sh
@@ -3,5 +3,7 @@
 # build minimal base image
 #
 pkg=locales,apt-utils,inetutils-ping,iproute2
+url=http://httpredir.debian.org/debian
+# export http_proxy
 
-/usr/share/docker-engine/contrib/mkimage.sh -t debian-base:latest -d . debootstrap --variant=minbase --components=main --include=$pkg jessie http://httpredir.debian.org/debian
+/usr/share/docker-engine/contrib/mkimage.sh -t debian-base:latest -d . debootstrap --variant=minbase --components=main --include=$pkg jessie $url
