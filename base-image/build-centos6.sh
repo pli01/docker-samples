dc731051c450f622325d2cfa06b05d167a128af5
pli01 <pli01@github.com>
2016-09-09 22:05:18 +0200

    centos mini

--- base-image/build-centos6.sh
+++ base-image/build-centos6.sh
@@ -6,9 +6,9 @@
 
 export distrib=centos
 export version=6
-export arch=6
+export arch=amd64
 export url=http://mirror.centos.org/centos/6/os/x86_64/Packages
 
-/usr/share/docker-engine/contrib/mkimage.sh -t $distrib$version-base:latest -d . rinse \
-  --distribution $distrib-$version --arch amd64   --verbose --mirror $url
+/home/b/mkimage/contrib/mkimage.sh -t $distrib$version-base:latest -d . rinse \
+  --distribution $distrib-$version --arch $arch   --verbose --mirror $url
 
