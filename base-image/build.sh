692c72995e9d648c57d925f6f6f0a800c0bfcb35
pli01 <pli01@github.com>
2016-09-10 09:46:04 +0200

    base debian

--- base-image/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# build minimal base image
-#
-pkg=locales,apt-utils,inetutils-ping,iproute2
-url=http://httpredir.debian.org/debian
-# export http_proxy
-
-/usr/share/docker-engine/contrib/mkimage.sh -t debian-base:latest -d . debootstrap --variant=minbase --components=main --include=$pkg jessie $url
